6077db34036a26e6814653745edbbb7544359259
Miguel Flórez <miguelfv10@hotmail.com>
2026-04-24 10:31:16 -0500

    FEAT: Se realiza la tarea 2 del Ejercicio Integrador 3
    Se desarrolla el item 2 del ejercicio integrador 3, correspondiente al desarrollo del ítem 1, pero utilizando promesas, en lugar de código síncrono. Así mismo, se importa la función y se reajusta el código de la primera función

--- Apropiación/Ej_Int3.js
+++ Apropiación/Ej_Int3.js
@@ -43,67 +43,209 @@ o Registrar tiempos reales con Date.now().
 o Contrastar con la ejecución basada en promesas.
 */
 
-const id = [101, 102, 103, 104];
+const consultaUsuarioBloqueante = () => {
+    const id = [101, 102, 103, 104];
 
-const roles = [
-    "Gerente General / CEO",
-    "Director Financiero / CFO",
-    "Director de Operaciones / COO",
-    "Gerente de Recursos Humanos",
-    "Gerente de Marketing",
-    "Gerente de Ventas",
-    "Gerente de Tecnología / CIO",
-    "Analista de Datos / BI",
-    "Supervisor de Producción",
-    "Asistente Administrativo"
-];
+    const roles = [
+        "Gerente General / CEO",
+        "Director Financiero / CFO",
+        "Director de Operaciones / COO",
+        "Gerente de Recursos Humanos",
+        "Gerente de Marketing",
+        "Gerente de Ventas",
+        "Gerente de Tecnología / CIO",
+        "Analista de Datos / BI",
+        "Supervisor de Producción",
+        "Asistente Administrativo"
+    ];
 
-const espera = (ms) => {
-    const start = Date.now();
+    const espera = (ms) => {
+        const start = Date.now();
 
-    while (Date.now() - start < ms) {
-        // Espera activa (bloqueante)
-    }
-}
-
-const consultaUsuarioBloqueante = (i) => {
-    const usuarios ={};
+        while (Date.now() - start < ms) {
+            // Espera activa (bloqueante)
+        }
+    };
 
     console.log("Demostración de código bloqueante");
 
-    console.log(`Consultando usuario ${i}`);
+    for (let i of id) {
+        console.log(`Consultando usuario ${i}`);
+        
+        espera(1200);
+
+        let nombre = `Usuario ${i}`;
+
+        console.log("Consultando seguridad");
+
+        espera(800);
+
+        let seguridad = Math.random() > 0.5 ? "OK" : "FALLA";
+
+        console.log("Consultando rol");
+
+        espera(2000);
+
+        let indice = Math.floor(Math.random() * roles.length);
+
+        let rol = roles[indice];
+
+        console.log("Generando registro");
+
+        espera(600);
+
+        console.log({
+            id: i,
+            nombre: nombre,
+            seguridad: seguridad,
+            rol: rol,
+            tiempoTotal: "4.6 segundos"
+        });
+    }
+
+    console.log("Este enfoque no funciona, debido a que el tiempo de registro, en caso de que se reciba una lista muy larga de usuarios, se haría extremadamente largo. En consecuencia, haría que la espera de los procesos se volviera un procedimiento insostenible en la práctica");
     
-    espera(1200);
-
-    let nombre = `Usuario ${i}`;
-
-    console.log("Consultando seguridad");
-
-    espera(800);
-
-    let seguridad = Math.random() > 0.5 ? "OK" : "FALLA";
-
-    console.log("Consultando rol");
-
-    espera(2000);
-
-    let indice = Math.floor(Math.random() * roles.length);
-
-    let rol = roles[indice];
-
-    espera(600);
-
-    console.log("Generando registro");
-
-    console.log({
-        id: id,
-        nombre: nombre,
-        seguridad: seguridad,
-        rol: rol,
-        tiempoTotal: "4.6 segundos"
-    });
 };
 
-const rdUser = id[Math.floor(Math.random() * id.length)];
+import promptSync from 'prompt-sync';
+const prompt = promptSync();
 
-export {consultaUsuarioBloqueante};
+const consultaUsuarioPromesa = () => {
+    let continuar = true;
+    
+    const id = [101, 102, 103, 104];
+
+    const roles = [
+        "Gerente General / CEO",
+        "Director Financiero / CFO",
+        "Director de Operaciones / COO",
+        "Gerente de Recursos Humanos",
+        "Gerente de Marketing",
+        "Gerente de Ventas",
+        "Gerente de Tecnología / CIO",
+        "Analista de Datos / BI",
+        "Supervisor de Producción",
+        "Asistente Administrativo"
+    ];
+    
+    const espera = (ms) => {
+        return new Promise((resolve) => setTimeout(resolve, ms));
+    };
+
+    while (continuar) {
+        console.log("Seleccione el usuario a registrar: \n1. 101 \n2. 102 \n3. 103 \n4. 104 \n5. No registrar");
+
+        const usreg = Number(prompt());
+
+        const select = id[usreg - 1];
+
+        switch (usreg) {
+            case 1:
+                let usuario1 = {id: select};
+
+                console.log(`Consultando usuario ${select}`);
+
+                espera(1200).then(() => {
+                    usuario1 = { ...usuario1, nombre: `Usuario ${select}` };
+
+                    espera(800).then(() => {
+                        usuario1 = { ...usuario1, seguridad: Math.random() > 0.5 ? "OK" : "FALLA"};
+
+                        espera(2000).then(() => {
+                            usuario1 = { ...usuario1, rol: roles[Math.floor(Math.random() * roles.length)]};
+
+                            espera(600).then(() => console.log(usuario1));
+
+                        });
+                    });
+                });
+
+                break;
+
+            case 2:
+                let usuario2 = {id: select};
+
+                console.log(`Consultando usuario ${select}`);
+
+                espera(1200).then(() => {
+                    usuario2 = { ...usuario2, nombre: `Usuario ${select}` };
+
+                    espera(800).then(() => {
+                        usuario2 = { ...usuario2, seguridad: Math.random() > 0.5 ? "OK" : "FALLA"};
+
+                        espera(2000).then(() => {
+                            usuario2 = { ...usuario2, rol: roles[Math.floor(Math.random() * roles.length)]};
+
+                            espera(600).then(() => console.log(usuario2));
+                            
+                        });
+                    });
+                });
+
+                break;
+
+            case 3:
+                let usuario3 = {id: select};
+
+                console.log(`Consultando usuario ${select}`);
+
+                espera(1200).then(() => {
+                    usuario3 = { ...usuario3, nombre: `Usuario ${select}` };
+
+                    espera(800).then(() => {
+                        usuario3 = { ...usuario3, seguridad: Math.random() > 0.5 ? "OK" : "FALLA"};
+
+                        espera(2000).then(() => {
+                            usuario3 = { ...usuario3, rol: roles[Math.floor(Math.random() * roles.length)]};
+
+                            espera(600).then(() => console.log(usuario3));
+                            
+                        });
+                    });
+                });
+
+                break;
+
+            case 4:
+                let usuario4 = {id: select};
+
+                console.log(`Consultando usuario ${select}`);
+
+                espera(1200).then(() => {
+                    usuario4 = { ...usuario4, nombre: `Usuario ${select}` };
+
+                    espera(800).then(() => {
+                        usuario4 = { ...usuario4, seguridad: Math.random() > 0.5 ? "OK" : "FALLA"};
+
+                        espera(2000).then(() => {
+                            usuario4 = { ...usuario4, rol: roles[Math.floor(Math.random() * roles.length)]};
+
+                            espera(600).then(() => console.log(usuario4));
+                            
+                        });
+                    });
+                });
+
+                break;
+        
+            case 5:
+                console.log("Terminando registros.");
+                
+                continuar = false;
+
+                return;
+            
+            default:
+                break;
+        };
+    
+    };
+
+};
+
+
+
+export {
+    consultaUsuarioBloqueante,
+    consultaUsuarioPromesa
+};
