4a5735701166efec5cdca6a308a3da708fa0c897
Miguel Flórez <miguelfv10@hotmail.com>
2026-04-26 14:07:44 -0500

    Se integra el ejercicio con app.js
    Se hace la integración del ejercicio integrador 2 con el menú del archivo app.js, por medio de la importación de la función correspondiente a través del archivo barril Index.js

--- Index.js
+++ Index.js
@@ -4,7 +4,8 @@ export * from "./Apropiación/Ej2.js";
 export * from "./Apropiación/Ej3.js";
 export * from "./Apropiación/Ej6.js";
 export * from "./Apropiación/Ej7.js";
-export * from "./Apropiación/Ej_Int1.js"
+export * from "./Apropiación/Ej_Int1.js";
+export * from "./Apropiación/Ej_Int2.js";
 export * from "./Apropiación/Ej_Int3.js";
 
 // Exportar ejercicios desde la carpeta de Transferencia
