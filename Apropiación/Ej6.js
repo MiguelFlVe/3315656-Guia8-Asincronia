1e1b89d4f21ee34b00e8c0fbe1d7c698f9cc5dd6
Miguel Flórez <miguelfv10@hotmail.com>
2026-04-26 12:33:13 -0500

    Se desarrolla el ejercicio 6 de Apropiación
    Se realiza el ejercicio 6 de Apropiación de la guía de Asincronía, consistente en la simulación de un proceso, con probabilidad del 50% de fallar, a través de promesas

---
+++ Apropiación/Ej6.js
@@ -0,0 +1,30 @@
+/* 
+Manejo de errores con Promesas
+Ejercicio: Crear una promesa que simule un proceso que puede fallar 50% de las veces usando resolve y reject.
+Meta: entender .catch() y la importancia del manejo de errores.
+*/
+
+const procesoConError = () => {
+    return new Promise((resolve, reject) => {
+        const exito = Math.random() > 0.5;
+
+        setTimeout(() => {
+            if (exito) {
+                resolve();
+            } else {
+                reject();
+            }
+        }, 2000);
+})};
+
+const ejecutarProceso = async () => {
+    await procesoConError()
+        .then(() => {
+            console.log('Proceso exitoso');
+        })
+        .catch(() => {
+            console.log('Ocurrió un error en el proceso');
+        });
+};
+
+ejecutarProceso();
