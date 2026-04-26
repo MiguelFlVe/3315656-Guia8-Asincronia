f09b1a587d56446ddadf07c4577d2df476a8d1c6
Oscar <mao090997@gmail.com>
2026-04-26 14:32:54 -0500

    feat: se agrega el enunciado del ejercicio 5 de apropiacion

--- Apropiación/Ej5.js
+++ Apropiación/Ej5.js
@@ -1,3 +1,8 @@
+// 5. Transformando Callbacks en Promesas
+// Ejercicio:
+// Convertir el ejercicio anterior en una estructura basada en Promesas con .then().
+// Meta: visualizar cómo mejora la legibilidad.
+
 // Función principal
 export const ejecutarPromesas = () => {
 
